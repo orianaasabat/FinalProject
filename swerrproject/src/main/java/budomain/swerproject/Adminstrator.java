5d94700d907bf2b3279c270163840841619af24a
orianaasabat <oriisbar1@gmail.com>
2018-05-12 14:45:47 +0300

    serch screen

---
+++ swerrproject/src/main/java/budomain/swerproject/Adminstrator.java
@@ -0,0 +1,21 @@
+package budomain.swerproject;
+
+
+
+class Adminstrator extends Person {
+	private String userName;
+	private String password;
+
+	public Adminstrator(String userName, String password) {
+		super();
+		this.userName = userName;
+		this.password = password;
+	}
+
+	public Adminstrator() {
+		super();
+	}
+
+}
+
+
---
+++ swerrproject/src/main/java/budomain/swerproject/Student.java
@@ -0,0 +1,32 @@
+package budomain.swerproject;
+
+public class Student extends Person {
+	int grade;
+	
+
+	public Student() {
+		super();
+	}
+
+	public Student(String name, int age, Address address, int grade) {
+		super(name, age, address);
+		this.grade = grade;
+
+	}
+
+	public int getGrade() {
+		return grade;
+	}
+
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [grade=" + grade + ", name=" + name + ", age=" + age + ", address: " + "\n city: "
+				+ address.getCity() + "\n house number " + address.getHouseNumber() + " \n Street name "
+				+ address.getStreetName() + "]";
+	}
+
+}
